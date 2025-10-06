4067eb3d9a8a47e566f936fb1be041795bd963fe
Shubham Sharma <124946918+the-shubham-sharma@users.noreply.github.com>
2025-10-07 00:18:23 +0530

    Update JournalEntryController

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.List; 
 import java.util.Map;
 
 @RestController
