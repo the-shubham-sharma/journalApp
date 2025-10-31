2d0450ad74988021b4b892ee4046934468cd2346
Shubham Sharma <124946918+the-shubham-sharma@users.noreply.github.com>
2025-10-31 22:47:53 +0530

    Update JournalEntryController.java

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List; 
 import java.util.Map;
 
-@RestController 
+@RestController  
 @RequestMapping("/journal")
 public class JournalEntryController {
  
