d57127df21ea8d2c97f00bfbed18ca10a024f312
Shubham Sharma <124946918+the-shubham-sharma@users.noreply.github.com>
2025-11-07 20:11:47 +0530

    Uncomment ResponseEntity import in JournalEntryController

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -2,7 +2,7 @@
 
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import org.springframework.http.HttpStatus;  
-//import org.springframework.http.ResponseEntity;
+//import org.springframework.http.ResponseEntity; 
 //import org.springframework.validation.annotation.Validated; 
 import org.springframework.web.bind.annotation.*;  
      
